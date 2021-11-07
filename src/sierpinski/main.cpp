67e8262cca716a928c8ee20c2eb2ac4e5979b53d
bubgit <ohernandezba@gmail.com>
2021-11-07 02:48:17 +0100

    sierpinski

--- src/sierpinski/main.cpp
+++ src/sierpinski/main.cpp
@@ -1,6 +1,108 @@
 #include <stdlib.h>
+#include <cstdio>
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#define GL_GLEXT_PROTOTYPES
+#define EGL_EGLEXT_PROTOTYPES
+#else
+//#include <glad/glad.h>
+#include <GL/glew.h>
+#endif
+#include <GLFW/glfw3.h>
+
+// Un arreglo de 3 vectores que representan 3 vértices
+static const GLfloat g_vertex_buffer_data[] = {
+    -1.0f,
+    -1.0f,
+    0.0f,
+    1.0f,
+    -1.0f,
+    0.0f,
+    0.0f,
+    1.0f,
+    0.0f,
+};
+
+GLFWwindow *window;
+GLuint vertexbuffer;
+
+void main_loop()
+{
+    // 1rst attribute buffer : vértices
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer(
+        0,        // atributo 0. No hay razón particular para el 0, pero debe corresponder en el shader.
+        3,        // tamaño
+        GL_FLOAT, // tipo
+        GL_FALSE, // normalizado?
+        0,        // Paso
+        (void *)0 // desfase del buffer
+    );
+    // Dibujar el triángulo !
+    glDrawArrays(GL_TRIANGLES, 0, 3); // Empezar desde el vértice 0S; 3 vértices en total -> 1 triángulo
+    glDisableVertexAttribArray(0);    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
+
+    /* Poll for and process events */
+    glfwPollEvents();
+}
 
 int main()
 {
+#ifndef __EMSCRIPTEN__
+    const char *description;
+#endif
+
+    /* Initialize the library */
+    if (!glfwInit())
+    {
+#ifndef __EMSCRIPTEN__
+        glfwGetError(&description);
+        printf("glfwInit error : %s,", description);
+#else
+        printf("glfwInit error,");
+#endif
+
+        return EXIT_FAILURE;
+    }
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+#ifndef __EMSCRIPTEN__
+        glfwGetError(&description);
+        printf("glfwInit error : %s,", description);
+#else
+        printf("glfwInit error,");
+#endif
+
+        glfwTerminate();
+
+        return EXIT_FAILURE;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    // Identificar el vertex buffer
+    GLuint vertexbuffer;
+    // Generar un buffer, poner el resultado en el vertexbuffer que acabamos de crear
+    glGenBuffers(1, &vertexbuffer);
+    // Los siguientes comandos le darán características especiales al 'vertexbuffer'
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    // Darle nuestros vértices a  OpenGL.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(main_loop, 0, true);
+#else
+    while (!glfwWindowShouldClose(window))
+        main_loop();
+#endif
+
+    glfwTerminate();
+
     return EXIT_SUCCESS;
 }
